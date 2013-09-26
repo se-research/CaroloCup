c51ff832863d48499ef938367d34ea89be074fdf
MuhanadNabeel <Muhanad_88@hotmail.se>
2013-09-26 11:55:06 +0200

    Added more code to the Calculation module

--- Tetrix/Calculations/steering.erl
+++ Tetrix/Calculations/steering.erl
@@ -4,10 +4,10 @@
 
 
 start() ->
-    
+    ok.
 
 init() ->
-    
+    ok.
 
 calculate({X1,Y1}, {X2,Y2}, {X3,Y3}, CarPos, CarHeading) ->
     case findcircle({X1,Y1}, {X2,Y2}, {X3,Y3}, CarPos, CarHeading) of 
@@ -20,24 +20,61 @@ calculate({X1,Y1}, {X2,Y2}, {X3,Y3}, CarPos, CarHeading) ->
     end.
 
 
-findcircle(P1, P2, P3, CarPos, CarHeading) -> Find
-	   P1 = node() -> {X1,Y1} -> {double,double}
-	   P2 = node() -> {X2,Y2} -> {double,double}
-           P3 = node() -> {X3,Y3} -> {double,double}
-	   CarPos = node() -> {X,Y} -> {double,double}
-	   CarHeading = node() -> {X,Y} -> {double,double}
-	   Radius = double
-           CenterPoint = node() -> {X,Y} -> {double,double}
-	   Find = {CenterPoint = node() -> {X,Y} , Radius = double}.
-           infinate = {CarPos,CarHeading,P3}
+findcircle({X1,Y1}, {X2,Y2}, {X3,Y3}, CarPos, CarHeading) -> 
+	   %{X1,Y1} -> {double,double},
+	   %{X2,Y2} -> {double,double},
+           %{X3,Y3} -> {double,double},
+	   %CarPos = node() -> {X,Y} -> {double,double},
+	   %CarHeading = node() -> {X,Y} -> {double,double},
+	   %Radius = double,
+           %CenterPoint = node() -> {X,Y} -> {double,double},
+	   %Find = {CenterPoint = node() -> {X,Y} , Radius = double},
+           %infinate = {CarPos,CarHeading,P3},
+        
+            Line1 = ((Y1 - Y2) / (X1 - X2)),
+            Line2 = ((Y2 - Y3) / (X2 - X3)),
+    
+            CenterPointX = (Line1*Line2*(Y3-Y1)+Line1*(X2+X3) - 
+		   Line2*(X1+X2))/(2*(Line1-Line2)),
+            CenterPointY = -(1/Line1)*(CenterPointX -
+		    ((X1+X2)/2) + (Y1 + Y2) / 2),
+            CenterPoint = {CenterPointX,CenterPointY},
+            Radius = getDistance(CenterPoint, {X1,Y1}),
 
+	    Line3 = getAng(X1 , Y1 , X2 , Y2),
+                    case Line3 > 100000 of 
+			true -> 
+			    infinite;
+			false ->
+			    Line4 = getAng(X2 , Y2 , X3 , Y3),	
+			    case Line4 > 100000 of
+				true ->
+				    infinite;
+				false ->
+				    Area = math:cos(Line4+(math:pi()/2-Line3)),
+				    {CeneterPoint , Radius}
+						
+		     end.	  
+           %Clockwise = Area/(abs(Area)),
+           %Circulate = 1.
+					    
 
-aim(CarPos, CarHeading, LastPoint) -> Aim				  
-	       Aim = {SteeringAng = double}.
+aim(CarPos, CarHeading, LastPoint) ->
+    
+    %Aim = {SteeringAng = double}.
+    ok.
 
+    
 
-followcircle(CarPos, CarHeading, CenterPoint, Radius) -> Aim
-	       Aim = {SteeringAng = double}.
+followcircle(CarPos, CarHeading, CenterPoint, Radius) -> 
+	       %Aim = {SteeringAng = double}.
+    %CenterAng = getAng(CarPos , CenterPoint),
+    %TangentAngOffset = Clockwise*math:pi()/2,
+    %LocationOffset = getDistance(CenterPoint , CarPos), 
+    %CorrectionAng = (1-(LocationOffset/Radius))*(Clockwise*math:pi()/2),
+    %{CenterAng + TangentAngOffset + CorrectionAng}.
+    
+
 	       
 getAng({X1,Y1} , {X2,Y2}) -> 
     math:atan(((Y2-Y1)/(X2-X1)) + 
