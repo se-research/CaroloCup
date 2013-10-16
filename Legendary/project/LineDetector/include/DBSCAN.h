2fe467f8e3b8f2d2af47427e2c9f6434ec48e586
Attila <nagat@chalmers.student.se>
2013-10-17 00:39:00 +0200

    one bug fix in DBSCAN, but still some other remained; finding solid and dash line takes forever most probably because of the remaining bugs in DBSCAN...

--- Legendary/project/LineDetector/include/DBSCAN.h
+++ Legendary/project/LineDetector/include/DBSCAN.h
@@ -8,16 +8,22 @@ using namespace std;
 
 class Dbscan {
 public:
-  Dbscan (vector<Point> *points, float eps, int minPts);
+  Dbscan (vector<Point>* points, float eps, int minPts);
   virtual ~Dbscan ();
   Clusters* getClusters();
 
 private:
   int m_minPts;
   float m_eps;
+  int m_c;
+  vector<Point>* m_points;
+  vector<bool> m_clustered;
+  vector<int> m_noise;
+  vector<bool> m_visited;
   Clusters m_clusters;
-  vector<int> regionQuery(vector<Point> *points, Point *keypoint, float eps);
-  void  calc(vector<Point> *points);
+  vector<int> regionQuery(Point& point);
+  void expandCluster(Point& point, vector<int>& nbPoints);
+  void calc();
 };
 
 #endif
--- Legendary/project/LineDetector/include/LineDetector.h
+++ Legendary/project/LineDetector/include/LineDetector.h
@@ -5,6 +5,7 @@
 #include "opencv2/opencv.hpp"
 #include "LineDetectorTypes.h"
 #include "DBSCAN.h"
+#include <numeric>
 
 using namespace cv;
 using namespace std;
@@ -14,20 +15,31 @@ public:
   // TODO: replace change esp to int for performanc reasons
   LineDetector(vector<Vec4i>& lines, float eps, int minPts);
   virtual ~LineDetector();
-  Line* getDashLine();
+  Line getDashLine();
+  Line getSolidLine();
 
   Clusters* getClusters(); // Attila: Only debugging
   /*
    * First is always the closer and second is the further line.
    */
-  std::pair<Line,Line> getSolidLine();
+  //std::pair<Line,Line> getSolidLine();
 
   Line getOptimalLine();
 
 private:
+  Line findDashLine();
+  Line findSolidLine();
+  int calcLength(const Point& p1, const Point& p2);
+  int calcLength(const Vec4i& v);
+  int calcStdev(vector<int>& v);
+  Line findBiggestDistance(Cluster& c);
+
   Dbscan* m_clusters;
   Line* m_dashLine;
   Line* m_solidLine;
+  int m_stdevTh;
+  int m_maxXTh;
+  int m_maxyTh;
 };
 
 #endif
