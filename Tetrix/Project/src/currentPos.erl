0e949797957c35fa271b1216140e3bf99027bf16
Snake <snake@localhost.localdomain>
2013-11-15 11:55:16 +0100

    Adding currentPos algo, alpha

---
+++ Tetrix/Project/src/currentPos.erl
@@ -0,0 +1,16 @@
+-module(currentPos).
+-compile(export_all).
+
+calculatePos(MegaSec, Sec, MiliSec, Speed, RazorData, SteeringRad, SteeringDeg, RazorInit, CarX, CarY)->
+
+    DeltaTime = (MegaSec*1000000+Sec+MiliSec/1000000)-(MegaSec*1000000+Sec+MiliSec/1000000),
+    
+    CorrectedSpeed = speedCorrection(Speed),
+
+    DeltaDistance = DeltaTime * CorrectedSpeed * 1000,
+    
+    CarHeading = RazorData - InitRazor + 90, 
+
+    RazorXPos = PrevX + (DeltaDistance * ( math:cos(CarHeading * math:pi()/180))),
+    
+    RazorYPos = PrevY + (DeltaDistance * ( math:sin(CarHeading * math:po()/180))).
