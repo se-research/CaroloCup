a7d0520686cb91da4bf9d0a7f91a10982f20a108
Viktor Botev <vici4a@gmail.com>
2014-02-02 01:40:36 +0100

    add neural network code

---
+++ Legendary/project/OpenDaVINCI-msv/apps/2013/CaroloCup/lanedetector/include/nnRoadSizeCalc.h
@@ -0,0 +1,24 @@
+/*
+ * nnRoadSizeCalc.h
+ *
+ * Code generation for function 'nnRoadSizeCalc'
+ *
+ * C source code generated on: Tue Jan 28 02:26:12 2014
+ *
+ */
+
+#ifndef __NNROADSIZECALC_H__
+#define __NNROADSIZECALC_H__
+/* Include files */
+#include <math.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "rtwtypes.h"
+#include "nnRoadAngleCalc_types.h"
+
+/* Function Declarations */
+extern void nnRoadSizeCalc(const double x1[3], double b_y1[3]);
+#endif
+/* End of code generation (nnRoadSizeCalc.h) */
