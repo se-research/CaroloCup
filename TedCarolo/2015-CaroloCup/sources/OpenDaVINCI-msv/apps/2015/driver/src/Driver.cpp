8b176935f84ffb0ef5ccb8381df857b779c807b2
Victor <victor.borosean@gmail.com>
2014-11-13 19:10:26 +0100

    Replace the driver to ver.1

--- TedCarolo/2015-CaroloCup/sources/OpenDaVINCI-msv/apps/2015/driver/src/Driver.cpp
+++ TedCarolo/2015-CaroloCup/sources/OpenDaVINCI-msv/apps/2015/driver/src/Driver.cpp
@@ -87,16 +87,107 @@ namespace msv {
 
                 // Design your control algorithm here depending on the input data from above.
 
+	if (carstate == 3) {
+	  
+		cerr << "parkingState: '" << parkingState << "'" << endl;
+	  
+		speed = 1.0;
+		
+		// Begin parking sequence once an object is detected
+		if (sbd.getDistance(2) > 0.0 && parkingState == -1) {
+			parkingState = 0;
+		}
+			
+	      
+		// start the timer when an empty space is detected
+		if (sbd.getDistance(2) < 0.0 && parkingState == 0) { 
+			parkingState = 1;
+			gettimeofday(&timer, 0);
+		}
+		
+		// start timer2 when object is detected
+		if (sbd.getDistance(2) > 0.0 && parkingState == 1) { 
+			parkingState = 2;
+			gettimeofday(&timer2, 0);
+			interval = 5500;
+		}
+		
+		// checking if the space is large enough for parking
+		
+		if (((timer2.tv_sec - timer.tv_sec) * 1000) + ((timer2.tv_usec - timer.tv_usec) / 1000) * speed > interval && parkingState == 2) { 
+			parkingState = 3;
+			interval = 12500;
+			gettimeofday(&timer, 0);
+		}
+		else if (((timer2.tv_sec - timer.tv_sec) * 1000) + ((timer2.tv_usec - timer.tv_usec) / 1000) * speed <= interval && parkingState == 2) {
+			parkingState = 0;
+		}
+		
+		// Turning right backwords to park
+		if (parkingState == 3) {
+		        gettimeofday(&timer2, 0);
+			TimeStamp currentTime;
+			speed = -0.5;
+			steering = 25 * Constants::DEG2RAD;
+ 			if (((timer2.tv_sec - timer.tv_sec) * 1000) + ((timer2.tv_usec - timer.tv_usec) / 1000) <= 900) {
+			  speed = 1.0;
+			}
+
+			if (((timer2.tv_sec - timer.tv_sec) * 1000) + ((timer2.tv_usec - timer.tv_usec) / 1000) <= 2000) {
+			  steering = 0 * Constants::DEG2RAD;
+			}
+
+			if (((timer2.tv_sec - timer.tv_sec) * 1000) + ((timer2.tv_usec - timer.tv_usec) / 1000) >= interval) { 
+			    interval = 12000 ;
+			    gettimeofday(&timer, 0);
+			    parkingState = 4;
+			}
+			cerr << "interval3: '" << (((timer2.tv_sec - timer.tv_sec) * 1000) + ((timer2.tv_usec - timer.tv_usec) / 1000)) << "'" << endl;
+		}
+		
+		
+
+		
+		// Straightening the car
+		if (parkingState == 4) {
+		        gettimeofday(&timer2, 0);
+			speed = -0.5;
+			steering = -25 * Constants::DEG2RAD;
+			if (((timer2.tv_sec - timer.tv_sec) * 1000) + ((timer2.tv_usec - timer.tv_usec) / 1000) * speed >= interval || (sbd.getDistance(0) < 0.5 && sbd.getDistance(1) > 0.0) ) { 
+			    speed = 0.0;
+			    parkingState = 5;
+			    
+			}
+		}
+		if (parkingState == 5) {
+			speed = 0.5;
+			steering = 25 * Constants::DEG2RAD;
+			if (abs(sbd.getDistance(0) - sbd.getDistance(1)) < 0.1) {
+			    speed = 0.0;
+			    parkingState = 6;
+			}
+		}
+		if (parkingState == 6) {
+			speed = -0.5;
+			steering = 0;
+			if (abs(sbd.getDistance(0)) < 1.0) {
+			    speed = 0.0;
+			}
+		}
+		
+
+
 
 		        // Create vehicle control data.
 		        VehicleControl vc;
+			TimeStamp start;
 
                 // With setSpeed you can set a desired speed for the vehicle in the range of -2.0 (backwards) .. 0 (stop) .. +2.0 (forwards)
-		        vc.setSpeed(2);
+		        vc.setSpeed(speed);
 
                 // With setSteeringWheelAngle, you can steer in the range of -26 (left) .. 0 (straight) .. +25 (right)
 //                 double desiredSteeringWheelAngle = 0; // 4 degree but SteeringWheelAngle expects the angle in radians!
-		        vc.setSteeringWheelAngle(25);
+		        vc.setSteeringWheelAngle(steering);
 
                 // You can also turn on or off various lights:
                 vc.setBrakeLights(false);
@@ -107,6 +198,7 @@ namespace msv {
 		        Container c(Container::VEHICLECONTROL, vc);
 		        // Send container.
 		        getConference().send(c);
+	        }
 
 	       
         }
