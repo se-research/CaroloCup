e01e75a32b1fa3d6ab09fb0af40e8ff46e78ad8c
ashfaq farooqui <ashfaq.farooqui@gmail.com>
2014-12-19 17:12:58 +0100

    Fixing arduino buffer in proxy

--- 2015-CaroloCup/sources/OpenDaVINCI-msv/apps/2015/proxy/src/sensorProtocol.cpp
+++ 2015-CaroloCup/sources/OpenDaVINCI-msv/apps/2015/proxy/src/sensorProtocol.cpp
@@ -36,20 +36,23 @@ using namespace core::wrapper;
         void sensorProtocol::receivedPartialString(const string &s) {
                   m_partialData.write(s.c_str(), s.length());
                   if(hasCompleteData()){
-                	  int pos=m_partialData.str().find('.')+1;
-                	  int length=m_partialData.str().find('.',pos)-pos;
+                    m_partialData.seekg(0, ios_base::beg);
+                    // cout<<"Recived New:"<<m_partialData.str()<<endl;
+                    int posFirst=m_partialData.str().find('.')+1;
+                	  int posSecond=m_partialData.str().find('.',posFirst);
+                    int length=posSecond-posFirst;
 //                	  cout<<"pos:"<<pos<<endl;
-//                	  cout<<"length:"<<length<<endl;
-                  string str=m_partialData.str().substr(pos+2,length);
-//                  cout<<"m_par:"<<m_partialData.str()<<endl;
-                	  invokeStringListener(str);
-
+               	  // cout<<"length:"<<length<<endl;
+                  invokeStringListener(m_partialData.str().substr(posFirst+2,length));
+                  // cout<<"m_par invoked:"<<m_partialData.str()<<endl;
+                  m_partialData.str(m_partialData.str().substr(posSecond));
+                  // cout<<"remaining data:"<<m_partialData.str()<<endl;
+                 
                 	  // After using str() to set the remaining string, the write pointer
                 	  // points to the beginning of the stream and further receivedString() calls
                 	  // would override existing data. So the write pointer has to point to the
                 	  // end of the stream.
-                	  m_partialData.str("");
-//                	  m_partialData.seekg(0, ios_base::end);
+                	  m_partialData.seekp(0, ios_base::end);
                   }
                }
 
@@ -58,7 +61,7 @@ using namespace core::wrapper;
             	   m_partialData.seekg(0, ios_base::end);
             	   const uint8_t streamSize = m_partialData.tellg();
             	   m_partialData.seekg(0, ios_base::beg);
-            	   if(streamSize>100){ //Currently 100 is used as a safety value to receive 3 lines from the sensor data, going ahead we need to make this more robust with some inherent logic
+            	   if(streamSize>60){ //Currently 100 is used as a safety value to receive 2 lines from the sensor data, going ahead we need to make this more robust with some inherent logic
             		   return true;
             	   }
             	   return false;
