2ba42f1f2312f53d475c91ac6354b5cdfa7b4317
Olof Ekelund <olof.ekelund84@gmail.com>
2014-12-21 18:29:27 +0100

    Update sensor_gathering.ino

--- 2015-CaroloCup/sources/Arduino/sensor_gathering/sensor_gathering.ino
+++ 2015-CaroloCup/sources/Arduino/sensor_gathering/sensor_gathering.ino
@@ -1,8 +1,7 @@
- #include <Wire.h>
+#include <Wire.h>
 #include <stdio.h>
 #define ADDRESSBACK 115
 #define ADDRESSFRONT 112
-//#define ADDRESSMIDDLE 114
 
 // WHEEL ENCODER
 #define WHEEL_DIAMETER            65.4
@@ -10,91 +9,73 @@
 #define WHEEL_CIRCUMFERENCE       WHEEL_DIAMETER * PI
 #define WHEEL_ENCODER_SEGMENTS    20
 #define DISTANCE_PER_SEGMENT      (float)WHEEL_CIRCUMFERENCE / WHEEL_ENCODER_SEGMENTS
-#define WHEEL_ENCODER_A_PIN       2
-#define WHEEL_ENCODER_B_PIN       3
-#define STOPPED_THRESHOLD_MICROS  2000000
-#define MICROS_TO_SECONDS         1000000
-#define MILLIMETER_TO_METER       1000
+#define WHEEL_ENCODER_PIN         0
 
-float velocity = 0.0;       // Velocity in meters / second
-long wheelEncoderLastTime = 0;
-int metersTraveled = 0;
-long distanceTravelledMilli = 0;
+// IR
+#define IR_MAP_SIZE               23
+#define IR_MAP_START_VALUE        3
 
-int irpin1 = 0;                 // analog pin used to connect the sharp sensor
-int irpin2 = 1;                 // analog pin used to connect the sharp sensor
-int irpin3 = 2;                 // analog pin used to connect the sharp sensor
-int irpin4 = 3;                 // analog pin used to connect the sharp sensor
+unsigned long distanceTravelledMilli = 0;
+
+int irLeftSide = 0;                  // analog pin used to connect the sharp sensor
+int irLeftBack = 1;                  // analog pin used to connect the sharp sensor
+int irRightBack = 2;                 // analog pin used to connect the sharp sensor
+int irRightSide = 3;                 // analog pin used to connect the sharp sensor
 int infraCount = 0;
 int uf = -1;
 int ub = -1;
 
-int val = 0,cm = 0;                 // variable to store the values from sensor(initially zero)
- // A normal distribution
- // out[] holds the values wanted in cm
-int out[] = { 30, 29, 28, 27, 26, 25, 24, 23, 22, 21, 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3  };  // 28
-  // note: the in array should have increasing values
-// in[] holds the measured analogRead() values for defined distances
-int in[]  = {27,31,35,39,43,48,56,65,77,85,93,101,113,121,135,150,165,181,203,220,245,275,310,350,400,460,560,630}; 
-int size = 28;
+int val = 0,cm = 0;                  // variable to store the values from sensor(initially zero)
 
+// All the input values for each of the sensors
+int irMapValues[4][23] = {{620, 489, 440, 380, 327, 308, 265, 259, 240, 225, 213, 203, 188, 180, 175, 168, 160, 159, 155, 152, 140, 127, 117}, 
+                          {600, 580, 474, 414, 356, 326, 289, 260, 246, 219, 203, 190, 185, 177, 170, 161, 156, 150, 144, 140, 134, 129, 125},
+                          {630, 543, 461, 400, 340, 300, 270, 243, 220, 200, 185, 175, 167, 150, 138, 128, 113, 108, 100, 95, 80, 76},
+                          {630, 620, 520, 430, 376, 335, 297, 268, 240, 224, 208, 195, 180, 172, 165, 160, 146, 134, 126, 120, 116, 107, 100}};
 int reading = 0;
 
 unsigned long time;
-/*unsigned long int cntF=0, cntR=0, cntOldF = 0, cntOldR = 0;
-int ms = 0;*/
 
 void setup()
 {
-  Serial.begin(115200);               // starts the serial monitor
+  Serial.begin(115200);
   Wire.begin();
   
-  attachInterrupt(0, encoderISR, CHANGE);
   
-  //attachInterrupt(0, countRotationsF, FALLING);
-  //attachInterrupt(1, countRotationsR, FALLING);
+  attachInterrupt(WHEEL_ENCODER_PIN, encoderISR, CHANGE);
 }
 
-int getDistance(int val, int sensorPin)
+int getDistanceIR(int val, int sensorPin)
 {
-  int returnVal = -1;
-  if(sensorPin == 1){
-      returnVal = 9963.2 * pow(val, -1.207);
-  }else if(sensorPin == 2){
-      returnVal = 1383 * pow(val, -0.897);
-  }else if(sensorPin == 3){
-      returnVal = 703.48 * pow(val, -0.771);
-  }else if(sensorPin == 0){
-      returnVal = 26181 * pow(val, -1.394);
+  for (int i = 0; i < IR_MAP_SIZE; i++) {
+    if (irMapValues[sensorPin][i] >= val && irMapValues[sensorPin][i + 1] < val) {
+      return IR_MAP_START_VALUE + i;
+    }
   }
-  return returnVal;
+  return -1;
+}
+
+int getDistanceUS(){
+  
 }
 
 void loop()
 {
   char irStr[13];
   if(infraCount >= 2){
-  int ir1, ir2, ir3, ir4;
-  ir1 = getDistance(analogRead(irpin1), 0);
-  ir2 = getDistance(analogRead(irpin2), 1);
-  ir3 = getDistance(analogRead(irpin3), 2);
-  ir4 = getDistance(analogRead(irpin4), 3); 
-  
-  sprintf(irStr, "i%2d,%2d,%2d,%2d", ir1, ir2, ir3, ir4);
-/*  Serial.print("i");
-  Serial.print(ir1);
-  Serial.print(",");
-  Serial.print(ir2);
-  Serial.print(",");
-  Serial.print(ir3);
-  Serial.print(",");
-  Serial.print(ir4);
-  Serial.println(".");*/
-  //Serial.println(irStr);
-  infraCount = 0;
+    int ir1 = getDistanceIR(analogRead(irLeftSide), 0);
+    int ir2 = getDistanceIR(analogRead(irLeftBack), 1);
+    int ir3 = getDistanceIR(analogRead(irRightBack), 2);
+    int ir4 = getDistanceIR(analogRead(irRightSide), 3);
+    
+    // Serial.println(getDistance(analogRead(irRightSide), 3));
+    sprintf(irStr, "i%2d,%2d,%2d,%2d", ir1, ir2, ir3, ir4);
+    
+    infraCount = 0;
   }
+  infraCount++;
   //instrumt sensor to read echoes
- Wire.beginTransmission(ADDRESSFRONT); // transmit to front sensor #112 (0x70)(It is actually 0xE0)
+  Wire.beginTransmission(ADDRESSFRONT); // transmit to front sensor #112 (0x70)(It is actually 0xE0)
                                // the address specified in the datasheet is 224 (0xE0)
                                // but i2c adressing uses the high 7 bits so it's 112
   Wire.write(byte(0x00));      // sets register pointer to the command register (0x00)  
@@ -105,15 +86,6 @@ void loop()
   Wire.write(byte(0x05));      // Write the minimum value 
   Wire.endTransmission();      // stop transmitting
   
-  /*Wire.beginTransmission(ADDRESSMIDDLE); // transmit to front sensor #112 (0x70)(It is actually 0xE0)
-  Wire.write(byte(0x00));      // sets register pointer to the command register (0x00)  
-  Wire.write(byte(0x51));      // command sensor to measure in "inches" (0x50) // use 0x51 for centimeters // use 0x52 for ping microseconds
-  Wire.write(byte(0x02));      // Write in location 2(Change Range)
-  Wire.write(byte(0x00));      // Write the minimum value
-  Wire.write(byte(0x01));      // write in location one (change gain)
-  Wire.write(byte(0x00));      // Write the minimum value                       
-  Wire.endTransmission();      // stop transmitting
-  */
   Wire.beginTransmission(ADDRESSBACK); // transmit to Back sensor #115 (0x73) (It is actually 0xE6)
   Wire.write(byte(0x00));      // sets register pointer to the command register (0x00)  
   Wire.write(byte(0x51));      // command sensor to measure in "inches" (0x50)   // use 0x51 for centimeters  // use 0x52 for ping microseconds
@@ -125,17 +97,13 @@ void loop()
   // wait for readings to happen
   delay(8);   // datasheet suggests at least 65 milliseconds
   
-  infraCount++;
 
-////////////////////////////////
- // instrumt sensor to return a particular echo reading
+  ////////////////////////////////
+  // instrumt sensor to return a particular echo reading
   Wire.beginTransmission(ADDRESSFRONT); // transmit to device #112
   Wire.write(byte(0x02));      // sets register pointer to echo #1 register (0x02)
   Wire.endTransmission(); // stop transmitting
-   /*Wire.beginTransmission(ADDRESSMIDDLE); // transmit to device #114
-  Wire.write(byte(0x02));      // sets register pointer to echo #1 register (0x02)
-  Wire.endTransmission();      // stop transmitting
- */
+  
   Wire.beginTransmission(ADDRESSBACK); // transmit to device #115
   Wire.write(byte(0x02));      // sets register pointer to echo #1 register (0x02)
   Wire.endTransmission();      // stop transmitting
@@ -144,41 +112,23 @@ void loop()
   Wire.requestFrom(ADDRESSFRONT, 2);    // request 2 bytes from slave device #112
   // step 5: receive reading from sensor
 
-  if(2<= Wire.available())    // if two bytes were received
+  if(2<= Wire.available())      // if two bytes were received
   {
-    reading = Wire.read();  // receive high byte (overwrites previous reading)
-    reading = reading << 8;    // shift high byte to be high 8 bits
-    reading |= Wire.read(); // receive low byte as lower 8 bits
+    reading = Wire.read();      // receive high byte (overwrites previous reading)
+    reading = reading << 8;     // shift high byte to be high 8 bits
+    reading |= Wire.read();     // receive low byte as lower 8 bits
     if(reading < 1000){
     uf = reading;
     }
   }
   
- /*     Wire.requestFrom(ADDRESSMIDDLE, 2);    // request 2 bytes from slave device #114
-  // step 5: receive reading from sensor
-    int um = -1;
-  
-  if(2<= Wire.available())    // if two bytes were received
-  {
-    reading = Wire.read();  // receive high byte (overwrites previous reading)
-    reading = reading << 8;    // shift high byte to be high 8 bits
-    reading |= Wire.read(); // receive low byte as lower 8 bits
-    //Serial.print("MIDDLE=");
-    //Serial.println(reading);   // print the reading
-    if(reading < 1000){
-    um = reading;
-    }
-  }*/
-  
-   Wire.requestFrom(ADDRESSBACK, 2);    // request 2 bytes from slave device #112
-  // step 5: receive reading from sensor
+  Wire.requestFrom(ADDRESSBACK, 2);    // request 2 bytes from slave device #112
 
-  if(2<= Wire.available())    // if two bytes were received
+  if(2<= Wire.available())      // if two bytes were received
   {
-    reading = Wire.read();  // receive high byte (overwrites previous reading)
-    reading = reading << 8;    // shift high byte to be high 8 bits
-    reading |= Wire.read(); // receive low byte as lower 8 bits
-    //if(reading < 1000 && reading != -1){
+    reading = Wire.read();      // receive high byte (overwrites previous reading)
+    reading = reading << 8;     // shift high byte to be high 8 bits
+    reading |= Wire.read();     // receive low byte as lower 8 bits
     if(reading < 1000){
 
     ub = reading;
@@ -186,64 +136,19 @@ void loop()
   }
 
   char uStr[13];
-
-  //sprintf(uStr, "u%3d,%3d,%3d", uf,um,ub);
-    sprintf(uStr, "u%3d,%3d", uf,ub);
+  
+  sprintf(uStr, "u%3d,%3d", uf,ub);
   char wStr[5];
   sprintf(wStr, "w%4d.",distanceTravelledMilli);
-  /*Serial.print("u");
-  Serial.print(uff);
-  Serial.print(",");
-  Serial.print(ubf);
-  Serial.println(".");*/
-  //Serial.println(uStr);
+  
 
   Serial.print(irStr);
   Serial.print(":");
   Serial.print(uStr);
   Serial.print(":");
   Serial.println(wStr);
-  
-  //sprintf(sndStr,"%d:%s",n,sndStr);
-  //Serial.println(sndStr);
-  /*ms++;
-  if(ms > 2) {
-    //Calculate car speed
-    unsigned long curr = millis();
-    Serial.print(cntF);
-    Serial.print(",");
-    Serial.println(cntR);
-    Serial.println((curr - time));
-    int diffF = cntF * 1000 / (curr - time);
-    int diffR = cntR * 1000 / (curr - time);
-    float carSpeedF = diffF * 0.03;
-    float carSpeedR = diffR * 0.03;
-    Serial.print("h");
-    Serial.print((int)(carSpeedF*100));
-    Serial.print(",");
-    Serial.print((int)(carSpeedR*100));
-    Serial.println(".");
-    ms=0;
-    cntF = 0;
-    cntR = 0;
-    time = curr;
-  }*/
 }
 
 void encoderISR(){
-  long newTime = micros();
-  float timePassed = (float)(newTime - wheelEncoderLastTime) / (float) MICROS_TO_SECONDS;
-
   distanceTravelledMilli += DISTANCE_PER_SEGMENT;
-  velocity = (DISTANCE_PER_SEGMENT / MILLIMETER_TO_METER) / timePassed;
-  wheelEncoderLastTime = newTime;
 }
-
-
-/*void countRotationsF() {
-  cntF++;
-}
-
-void countRotationsR() {
-  cntR++;
-}*/
