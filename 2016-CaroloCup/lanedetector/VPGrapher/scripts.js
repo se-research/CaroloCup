40d272418908518efad84ffb565011cbda6761e3
Simeon Ivanov <simeon@ivanovsimeon.com>
2015-11-13 19:34:03 +0100

    Remove histogram

--- 2016-CaroloCup/lanedetector/VPGrapher/scripts.js
+++ 2016-CaroloCup/lanedetector/VPGrapher/scripts.js
@@ -29,41 +29,6 @@ __VPGrapher = {
         });
     },
     draw: {
-        histogram: function(data, svg) {
-           var frames = data.length;
-
-           var margin = {top: 20, right: 20, bottom: 30, left: 50},
-               width = 960 - margin.left - margin.right,
-               height = 500 - margin.top - margin.bottom;
-
-           var x = d3.scale.linear()
-               .domain([0, d3.max(data)])
-               .range([0, width]);
-
-           data = d3.layout.histogram().bins(x.ticks(20))(data);
-
-           var y = d3.scale.linear()
-               .domain([0, frames])
-               .range([height, 0]);
-
-           var bar = svg.selectAll(".bar")
-               .data(data)
-               .enter().append("g")
-               .attr("class", "bar")
-               .attr("transform", function(d) { return "translate(" + x(d.x) + "," + y(d.y) + ")"; });
-
-           bar.append("rect")
-               .attr("x", 1)
-               .attr("width", x(data[0].dx) - 1)
-               .attr("height", function(d) { return height - y(d.y); });
-
-           bar.append("text")
-               .attr("dy", ".75em")
-               .attr("y", 6)
-               .attr("x", x(data[0].dx) / 2)
-               .attr("text-anchor", "middle")
-               .text(function(d) { return d.y; });
-        },
         normalDistributionCDF: function(originalData) {
             var frames = originalData.length;
 
@@ -123,8 +88,6 @@ __VPGrapher = {
                 .attr("height", height + margin.top + margin.bottom)
                 .append("g")
                 .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-            __VPGrapher.draw.histogram(originalData, svg);
 
             svg.append("g")
                 .attr("class", "x axis")
