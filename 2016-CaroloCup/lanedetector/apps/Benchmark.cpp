abaabc1ebdf04f7c75ddf44b9048b629d4d7dcc0
Simeon Ivanov <simeon@ivanovsimeon.com>
2015-12-11 12:33:58 +0100

    Remove sleep after compiling

--- 2016-CaroloCup/lanedetector/apps/Benchmark.cpp
+++ 2016-CaroloCup/lanedetector/apps/Benchmark.cpp
@@ -7,7 +7,6 @@ int main(int argc, char** argv) {
 
     if (argc != 2) {
         compileBranch();
-        sleep(1);
         setupTempFolder();
         processScenarios("primary");
         removeTempFolder();
