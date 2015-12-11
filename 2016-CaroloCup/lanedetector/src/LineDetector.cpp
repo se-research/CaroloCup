63dd87f6de315e6bc5fc300e3c61dcfff3a73060
Ibtissam Karouach <guskarib@student.gu.se>
2015-12-11 23:30:09 +0100

    Test threshold

--- 2016-CaroloCup/lanedetector/src/LineDetector.cpp
+++ 2016-CaroloCup/lanedetector/src/LineDetector.cpp
@@ -61,7 +61,7 @@ LineDetector::LineDetector(const Mat &f, const Config &cfg, const bool debug,
     h = m_frame.size().height;
     offset = 2 * h / 16 - 1;
     /// Detect edges using Threshold
-    threshold(m_frame, m_frame, 0, 255, CV_THRESH_BINARY);
+    threshold(m_frame, m_frame, 150, 255, CV_THRESH_BINARY);
     cvtColor(m_frame, m_frame_color, CV_GRAY2BGR);
 
     // Run lineDetector and provide goalLines for the driver
