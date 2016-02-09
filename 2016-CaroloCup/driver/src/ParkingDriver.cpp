dfa78d768e6d4574d9fb451aaa22c094ce797f8a
DigitalGabriele <mfourgeaud@gmail.com>
2016-02-09 03:55:49 +0100

    Modified count of max try to set alignment

--- 2016-CaroloCup/driver/src/ParkingDriver.cpp
+++ 2016-CaroloCup/driver/src/ParkingDriver.cpp
@@ -458,7 +458,7 @@ namespace msv {
                 }
                 else*/
 
-                if ( ((USFront > 0) && (USRear > 0) && ((USFront + USRear) <= USStraight)) || count >= 2) {
+                if ( ((USFront > 0) && (USRear > 0) && ((USFront + USRear) <= USStraight)) || count >= 1) {
                     USStrCheck = (USFront + USRear);
                     parking_state = STOP;
                     desiredSpeed = Stop_Speed_Backward;
@@ -480,7 +480,7 @@ namespace msv {
                 cout << "\t\t========  FORWARD_RIGHT" << endl;
                 int FrontStopLimit = 14;
 
-                if (((USFront > 0) && (USRear > 0) && ((USFront + USRear) <= USStraight)) || count >= 2) {
+                if (((USFront > 0) && (USRear > 0) && ((USFront + USRear) <= USStraight)) || count >= 1) {
                     USStrCheck = (USFront + USRear);
                     desiredSpeed = Stop_Speed_Backward;
                     parking_state = STOP;
